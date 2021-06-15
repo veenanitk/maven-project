433dcf3efb54c9fccc8ff06eb2b12eec3b3ee1bf
Veena <veenagar@publicisgroupe.net>
2021-06-15 15:56:18 +0530

    added

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    //private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        //Logger log = LoggerFactory.getLogger(App.class);
+        System.out.println( "Java is calling you! WAKE UP" );
+        //log.info("Thi is debug log");
+
+    }
+}
